70742cb2aca4eadcf5bfccae229a3b7174edbe7f
Artemii <artemiithefrog95@icloud.com>
2024-04-02 14:53:03 +0200

    Initial Commit

---
+++ WithUWitdget/WithUWitdgetApp.swift
@@ -0,0 +1,17 @@
+//
+//  WithUWitdgetApp.swift
+//  WithUWitdget
+//
+//  Created by artemiithefrog . on 02.04.2024.
+//
+
+import SwiftUI
+
+@main
+struct WithUWitdgetApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
