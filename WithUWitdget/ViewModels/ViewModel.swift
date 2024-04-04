9db0921b110960a23e2145bbd3760530b5d6f026
Artemii <artemiithefrog95@icloud.com>
2024-04-04 22:45:17 +0200

    ViewModel

--- WithUWitdget/ViewModels/ViewModel.swift
+++ WithUWitdget/ViewModels/ViewModel.swift
@@ -5,10 +5,14 @@
 //  Created by artemiithefrog . on 02.04.2024.
 //
 
-import Foundation
+import SwiftUI
 
 class ViewModel: ObservableObject {
 
-  @Published var leftCat: String = "cat1"
-  @Published var leftCat: String = "cat2"
+  @AppStorage("leftCat", store: UserDefaults(suiteName: "group.artemiithefrog.WithUWitdget"))
+  var leftCat = "cat1"
+  @AppStorage("rightCat", store: UserDefaults(suiteName: "group.artemiithefrog.WithUWitdget"))
+  var rightCat = "cat1"
+
+  var cats: [Cats] = [Cats(name: "cat1"),Cats(name: "cat2"),Cats(name: "cat3"),Cats(name: "cat4"),Cats(name: "cat5")]
 }
